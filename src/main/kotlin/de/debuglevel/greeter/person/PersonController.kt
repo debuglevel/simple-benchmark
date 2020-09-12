8786b5a1d67a08d1164fa8e66a0f946f51ed961f
Marc Kohaupt <debuglevel@gmail.com>
2020-09-12 14:17:50 +0200

    Initial commit

---
+++ src/main/kotlin/de/debuglevel/greeter/person/PersonController.kt
@@ -0,0 +1,132 @@
+package de.debuglevel.greeter.person
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Put
+import io.micronaut.http.server.types.files.StreamedFile
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
+import io.swagger.v3.oas.annotations.tags.Tag
+import mu.KotlinLogging
+import java.util.*
+
+@Secured(SecurityRule.IS_ANONYMOUS)
+@Controller("/persons")
+@Tag(name = "persons")
+class PersonController(private val personService: PersonService) {
+    private val logger = KotlinLogging.logger {}
+
+    /**
+     * Get all persons
+     * @return All persons
+     */
+    @Get("/")
+    fun getAll(): HttpResponse<*> {
+        logger.debug("Called getAll()")
+        return try {
+            val persons = personService.list()
+            val getPersonResponses = persons
+                .map { GetPersonResponse(it) }
+
+            HttpResponse.ok(getPersonResponses)
+        } catch (e: Exception) {
+            logger.error(e) { "Unhandled exception" }
+            HttpResponse.serverError("Unhandled exception: ${e.message}")
+        }
+    }
+
+    /**
+     * Get a person
+     * @param id ID of the person
+     * @return A person
+     */
+    @Get("/{id}")
+    fun getOne(id: UUID): HttpResponse<*> {
+        logger.debug("Called getOne($id)")
+        return try {
+            val person = personService.get(id)
+
+            val getPersonResponse = GetPersonResponse(person)
+            HttpResponse.ok(getPersonResponse)
+        } catch (e: PersonService.EntityNotFoundException) {
+            logger.debug { "Getting person $id failed: ${e.message}" }
+            HttpResponse.notFound("Person $id does not exist.")
+        } catch (e: Exception) {
+            logger.error(e) { "Unhandled exception" }
+            HttpResponse.serverError("Unhandled exception: ${e.message}")
+        }
+    }
+
+    /**
+     * Download a never ending file of random names
+     */
+    @Get("/endlessRandom")
+    fun downloadRandomEndless(): StreamedFile {
+        logger.debug("Called downloadRandomEndless()")
+
+        val inputStream = personService.randomStream()
+        return StreamedFile(inputStream, MediaType.TEXT_PLAIN_TYPE)
+    }
+
+    /**
+     * Create a person.
+     * @return A person with their ID
+     */
+    @Post("/")
+    fun postOne(addPersonRequest: AddPersonRequest): HttpResponse<*> {
+        logger.debug("Called postOne($addPersonRequest)")
+
+        return try {
+            val person = addPersonRequest.toPerson()
+            val addedPerson = personService.add(person)
+
+            val addPersonResponse = AddPersonResponse(addedPerson)
+            HttpResponse.created(addPersonResponse)
+        } catch (e: Exception) {
+            logger.error(e) { "Unhandled exception" }
+            HttpResponse.serverError("Unhandled exception: ${e.message}")
+        }
+    }
+
+    /**
+     * Update a person.
+     * @param id ID of the person
+     * @return The updated person
+     */
+    @Put("/{id}")
+    fun putOne(id: UUID, updatePersonRequest: UpdatePersonRequest): HttpResponse<*> {
+        logger.debug("Called putOne($id, $updatePersonRequest)")
+
+        return try {
+            val person = updatePersonRequest.toPerson()
+            val updatedPerson = personService.update(id, person)
+
+            val updatePersonResponse = UpdatePersonResponse(updatedPerson)
+            HttpResponse.ok(updatePersonResponse)
+        } catch (e: PersonService.EntityNotFoundException) {
+            logger.debug { "Updating person $id failed: ${e.message}" }
+            HttpResponse.notFound("Person $id does not exist.")
+        } catch (e: Exception) {
+            logger.error(e) { "Unhandled exception" }
+            HttpResponse.serverError("Unhandled exception: ${e.message}")
+        }
+    }
+
+    /**
+     * Get all VIPs
+     * @return All VIPs
+     */
+    @Secured(SecurityRule.IS_AUTHENTICATED)
+    @Get("/VIPs")
+    fun getVIPs(): Set<GetPersonResponse> {
+        logger.debug("Called getVIPs()")
+        return setOf(
+            GetPersonResponse(UUID.randomUUID(), "Harry Potter"),
+            GetPersonResponse(UUID.randomUUID(), "Hermoine Granger"),
+            GetPersonResponse(UUID.randomUUID(), "Ronald Weasley")
+        )
+    }
+}
---
+++ src/main/kotlin/de/debuglevel/greeter/person/PersonService.kt
@@ -0,0 +1,561 @@
+package de.debuglevel.greeter.person
+
+import mu.KotlinLogging
+import java.io.InputStream
+import java.io.PipedInputStream
+import java.io.PipedOutputStream
+import java.util.*
+import javax.inject.Singleton
+import kotlin.concurrent.thread
+
+@Singleton
+class PersonService(
+    private val personRepository: PersonRepository
+) {
+    private val logger = KotlinLogging.logger {}
+
+    fun get(id: UUID): Person {
+        logger.debug { "Getting person with ID '$id'..." }
+
+        val person: Person = personRepository.findById(id).orElseThrow {
+            logger.debug { "Getting person with ID '$id' failed" }
+            EntityNotFoundException(id)
+        }
+
+        logger.debug { "Got person with ID '$id': $person" }
+        return person
+    }
+
+    fun add(person: Person): Person {
+        logger.debug { "Adding person '$person'..." }
+
+        val savedPerson = personRepository.save(person)
+
+        logger.debug { "Added person: $savedPerson" }
+        return savedPerson
+    }
+
+    fun update(id: UUID, person: Person): Person {
+        logger.debug { "Updating person '$person' with ID '$id'..." }
+
+        // an object must be known to Hibernate (i.e. retrieved first) to get updated;
+        // it would be a "detached entity" otherwise.
+        val updatePerson = this.get(id).apply {
+            name = person.name
+        }
+
+        val updatedPerson = personRepository.update(updatePerson)
+
+        logger.debug { "Updated person: $updatedPerson with ID '$id'" }
+        return updatedPerson
+    }
+
+    fun list(): Set<Person> {
+        logger.debug { "Getting all persons ..." }
+
+        val persons = personRepository.findAll().toSet()
+
+        logger.debug { "Got all persons" }
+        return persons
+    }
+
+    private fun generateRandom(): Person {
+        logger.debug { "Generating random person..." }
+        val person = Person(UUID.randomUUID(), "${firstnames.random()} ${lastnames.random()}")
+        logger.debug { "Generated random person: $person" }
+        return person
+    }
+
+    fun randomStream(): InputStream {
+        val outputStream = PipedOutputStream()
+
+        val inputStream = PipedInputStream()
+        inputStream.connect(outputStream)
+
+        thread(start = true) {
+            logger.debug { "Thread ${Thread.currentThread()} started." }
+
+            var pipeOpen = true
+            while (pipeOpen) {
+                logger.debug { "Thread ${Thread.currentThread()} sleeping..." }
+                Thread.sleep(500)
+
+                val name = generateRandom().name
+                val byteArray = "$name\n".toByteArray()
+
+                try {
+                    logger.debug { "Writing '$name' to OutputStream..." }
+                    outputStream.write(byteArray)
+                    logger.debug { "Wrote '$name' to OutputStream." }
+                } catch (e: Exception) {
+                    logger.warn(e) { "Writing to OutputStream failed" }
+                    pipeOpen = false
+                }
+            }
+
+            logger.debug { "Pipe was closed; Thread is ending..." }
+        }
+
+        return inputStream
+    }
+
+    private val lastnames = arrayOf(
+        "Georgeanna",
+        "Seraphim",
+        "Drus",
+        "Thalia",
+        "Jilly",
+        "Haymes",
+        "Tanberg",
+        "Wyly",
+        "Holofernes",
+        "Anastatius",
+        "Middendorf",
+        "Eirena",
+        "Henrieta",
+        "Pavyer",
+        "Trevorr",
+        "Edgerton",
+        "Ynes",
+        "Aldridge",
+        "Ole",
+        "Childs",
+        "Baryram",
+        "Henryetta",
+        "Erhard",
+        "Bautram",
+        "New",
+        "Gabbert",
+        "Knuth",
+        "Ignacia",
+        "Hawkie",
+        "Suzetta",
+        "Timothy",
+        "Egidius",
+        "Joshuah",
+        "Melmon",
+        "Mick",
+        "Toolis",
+        "Pain",
+        "Rausch",
+        "Vere",
+        "Pettit",
+        "Kraul",
+        "Lori",
+        "Apfelstadt",
+        "Ozmo",
+        "Horlacher",
+        "Ddene",
+        "Askwith",
+        "Darrelle",
+        "Kristos",
+        "September",
+        "Vincentia",
+        "Decamp",
+        "Anagnos",
+        "Yeo",
+        "Camel",
+        "Alleyne",
+        "Halbert",
+        "Isidro",
+        "Brodie",
+        "Pattie",
+        "Atthia",
+        "Greene",
+        "Paule",
+        "Weinert",
+        "Chery",
+        "Oby",
+        "Machute",
+        "Forbes",
+        "Herzog",
+        "Ratib",
+        "Madel",
+        "Axe",
+        "Bradley",
+        "Beisel",
+        "Buatti",
+        "Callahan",
+        "Ronny",
+        "Lathe",
+        "Eolanda",
+        "Stetson",
+        "Banky",
+        "Lorrimer",
+        "Jacobba",
+        "Landbert",
+        "Connett",
+        "Phyllys",
+        "Chaker",
+        "Peggir",
+        "Arndt",
+        "Kimberli",
+        "Frants",
+        "Ofella",
+        "Mun",
+        "Aekerly",
+        "Raynah",
+        "Om",
+        "Belen",
+        "Truman",
+        "Henryson",
+        "Fannie",
+        "Dottie",
+        "Klarrisa",
+        "Idette",
+        "Anselma",
+        "Hildegaard",
+        "Reinhold",
+        "Boorman",
+        "Bodi",
+        "Pryor",
+        "Oleta",
+        "Denni",
+        "Rothwell",
+        "Grindlay",
+        "Lovel",
+        "Capps",
+        "Ananna",
+        "Alexei",
+        "Ching",
+        "Arria",
+        "Dougy",
+        "Langer",
+        "Caddaric",
+        "Carree",
+        "Pry",
+        "Britta",
+        "Fan",
+        "Brest",
+        "Bullion",
+        "Annadiana",
+        "Aloisia",
+        "Malory",
+        "Japha",
+        "Marshal",
+        "Julianne",
+        "Gleeson",
+        "Willtrude",
+        "Royd",
+        "Tillinger",
+        "Borgeson",
+        "Kenneth",
+        "Ranie",
+        "Seymour",
+        "Anett",
+        "Philander",
+        "Nicholl",
+        "Wetzell",
+        "Fadiman",
+        "Cathleen",
+        "Pinkham",
+        "Dulci",
+        "Saphra",
+        "Jezabella",
+        "Elvina",
+        "Sheeran",
+        "Shute",
+        "Carman",
+        "Krisha",
+        "Une",
+        "Jone",
+        "Cayser",
+        "Weinreb",
+        "Steve",
+        "Gilda",
+        "Burns",
+        "Bertie",
+        "Blockus",
+        "Myer",
+        "Gilemette",
+        "Kylander",
+        "Libbie",
+        "Anderea",
+        "Kristof",
+        "Janey",
+        "Layney",
+        "Urban",
+        "Herb",
+        "Nertie",
+        "Salvay",
+        "Bucher",
+        "Soneson",
+        "Zeiger",
+        "Augustus",
+        "Bainter",
+        "Zildjian",
+        "Esbenshade",
+        "Madalyn",
+        "Cralg",
+        "Adamec",
+        "Azelea",
+        "Montagna",
+        "Garmaise",
+        "Meneau",
+        "Wait",
+        "Corron",
+        "Warp",
+        "Jude",
+        "Moule",
+        "Damita",
+        "Guinn",
+        "Ulrika",
+        "Kimitri",
+        "Wengert",
+        "Dannel",
+        "Lauer",
+        "Bowie",
+        "Betsy",
+        "Cranston",
+        "Juanne",
+        "Sedberry",
+        "Smail",
+        "Shulamith",
+        "Kiley",
+        "Alyworth",
+        "Lauzon",
+        "Laughton",
+        "Yule",
+        "Quint",
+        "Bonnie",
+        "Clawson",
+        "Paza",
+        "Doty"
+    )
+
+    private val firstnames = arrayOf(
+        "Jackqueline",
+        "Dorothy",
+        "Annelise",
+        "Dot",
+        "Joana",
+        "Moll",
+        "Linet",
+        "Kaitlyn",
+        "Ernestine",
+        "Milzie",
+        "Casandra",
+        "Jaquelyn",
+        "Pammi",
+        "Laurene",
+        "Kyla",
+        "Zorah",
+        "Anthe",
+        "Jazmin",
+        "Odette",
+        "Jonell",
+        "Cassi",
+        "Venita",
+        "Tuesday",
+        "Robby",
+        "Agretha",
+        "Binnie",
+        "Gipsy",
+        "Eadith",
+        "Ianthe",
+        "Cherianne",
+        "Fredia",
+        "Risa",
+        "Liv",
+        "Faythe",
+        "Stella",
+        "Ginnifer",
+        "Marj",
+        "Lexine",
+        "Joey",
+        "Orelee",
+        "Ethyl",
+        "Sibel",
+        "Shaylah",
+        "Vi",
+        "Brigit",
+        "Noelyn",
+        "Delores",
+        "Madalena",
+        "Clarissa",
+        "Misha",
+        "Teodora",
+        "Anthea",
+        "Lyndsie",
+        "Laney",
+        "Evvie",
+        "Bette",
+        "Caitlin",
+        "Lissie",
+        "Rebekah",
+        "Rachelle",
+        "Latrina",
+        "Belva",
+        "Lise",
+        "Matti",
+        "Liza",
+        "Amalie",
+        "Melli",
+        "Rivalee",
+        "Shana",
+        "Yetta",
+        "Marrissa",
+        "Ninnette",
+        "Robyn",
+        "Matilda",
+        "Norrie",
+        "Darice",
+        "Denyse",
+        "Mirella",
+        "Herta",
+        "Glennie",
+        "Selinda",
+        "Felicdad",
+        "Nomi",
+        "Cassandre",
+        "Tilly",
+        "Anastasia",
+        "Kary",
+        "Breena",
+        "Sharona",
+        "Ailyn",
+        "Tildi",
+        "Georgeanne",
+        "Filia",
+        "Aggy",
+        "Bobine",
+        "Dorolice",
+        "Fionna",
+        "Loria",
+        "Adrienne",
+        "Martita",
+        "Lexi",
+        "Corabel",
+        "Fanchette",
+        "Heidi",
+        "Ceil",
+        "Nertie",
+        "Gavra",
+        "Poppy",
+        "Kattie",
+        "Yasmin",
+        "Cassandra",
+        "Bernelle",
+        "Juli",
+        "Cheri",
+        "Dasha",
+        "Sheena",
+        "Fredericka",
+        "Lola",
+        "Barbi",
+        "Katina",
+        "Vanda",
+        "Melitta",
+        "Vallie",
+        "Stace",
+        "Thomasin",
+        "Koral",
+        "Odele",
+        "Tybie",
+        "Caprice",
+        "Miriam",
+        "Cecil",
+        "Krystal",
+        "Kandy",
+        "Neysa",
+        "Caralie",
+        "Hedvige",
+        "Ingaborg",
+        "Daisy",
+        "Shanon",
+        "Chrissie",
+        "Lee",
+        "Kriste",
+        "Virginia",
+        "Maggee",
+        "Kelsey",
+        "Lucienne",
+        "Valerie",
+        "Dacie",
+        "Chelsea",
+        "Felice",
+        "Helen-Elizabeth",
+        "Alvinia",
+        "Orelle",
+        "Merrielle",
+        "Nancie",
+        "Ardisj",
+        "Jocelyn",
+        "Hayley",
+        "Ray",
+        "Thekla",
+        "Shawnee",
+        "Dagmar",
+        "Dulcia",
+        "Ofelia",
+        "Fanya",
+        "Maura",
+        "Oliy",
+        "Roanna",
+        "Magdalene",
+        "Mallory",
+        "Concordia",
+        "Candis",
+        "Arliene",
+        "Helge",
+        "Marci",
+        "Alissa",
+        "Venus",
+        "Anstice",
+        "Nita",
+        "Mavis",
+        "Elsa",
+        "Blake",
+        "Nanni",
+        "Mahalia",
+        "Quintina",
+        "Delilah",
+        "Kristien",
+        "Amandie",
+        "Bridie",
+        "Willow",
+        "Clovis",
+        "Drucie",
+        "Blinny",
+        "Glynnis",
+        "Dodi",
+        "Angelia",
+        "Mella",
+        "Orel",
+        "Danice",
+        "Edi",
+        "Serene",
+        "Deeanne",
+        "Loreen",
+        "Wenona",
+        "Celina",
+        "Renae",
+        "Emelita",
+        "Cristy",
+        "Leela",
+        "Cristin",
+        "Lyssa",
+        "Cele",
+        "Daisi",
+        "Kore",
+        "Konstance",
+        "Nettle",
+        "Adina",
+        "Daisie",
+        "Binni",
+        "Celie",
+        "Beverly",
+        "Laraine",
+        "Milissent",
+        "Edeline",
+        "Antonetta",
+        "Helena",
+        "Collette",
+        "Pepi",
+        "Christi",
+        "Junie",
+        "Jess"
+    )
+
+    class EntityNotFoundException(criteria: Any) : Exception("Entity '$criteria' does not exist.")
+}
